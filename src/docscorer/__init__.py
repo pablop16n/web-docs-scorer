e234e3353e2927ed9018b1eadbb357c913ac50de
Marta Bañón <mbanon@prompsit.com>
2025-11-10 17:22:28 +0100

    Fixes for proper installation with pip

--- src/docscorer/__init__.py
+++ src/docscorer/__init__.py
@@ -3,8 +3,9 @@
 from importlib.metadata import version
 
 from docscorer.docscorer import DocumentScorer
+from docscorer.configuration import ScorerConfiguration
 
 name = "docscorer"
 __version__ = version(name)
 
-__all__ = ["DocumentScorer"]
+__all__ = ["DocumentScorer", "ScorerConfiguration"]
